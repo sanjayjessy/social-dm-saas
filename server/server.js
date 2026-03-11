647308b2b66ddf4684d2724419cdeef3c0563d58
sanjayjessy <codingweb080@gmail.com>
2026-03-11 12:39:57 +0530

    update

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://cmcpro.netlify.app'
+    'https://cmcpro.netlify.app',
+    'https://procmc.netlify.app'
   ],
   credentials: true
 }));
